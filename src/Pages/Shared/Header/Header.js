ea6774d088ff9bdde72685eefa6f911eab678b65
mukitcoder <mukitbd71@gmail.com>
2021-11-22 12:46:20 +0600

    adjust header alignments

--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -17,7 +17,7 @@ const Header = () => {
           <Navbar.Toggle aria-controls="navbarScroll" />
           <Navbar.Collapse id="navbarScroll">
             <Nav
-              className="mx-auto my-2 my-lg-0 fw-bold"
+              className="mx-auto my-lg-0 fw-bold"
               style={{ maxHeight: "300px" }}
               navbarScroll
             >
@@ -36,19 +36,17 @@ const Header = () => {
               <Nav.Link className="text-white" as={HashLink} to="/about">
                 About Us
               </Nav.Link>
-              <Navbar.Text className="ms-3 ">
-                <img width="30px" className="img-fluid rounded-circle" src= {user?.photoURL} alt="" />
-              </Navbar.Text>
-              <Navbar.Text className="mx-1 ">
+              <div className="ms-3 d-flex justify-content-center align-items-center">
+                <img width="30px" className="img-fluid rounded-circle me-2" src= {user?.photoURL} alt="" />
                 <a className="text-white" href="#login">{user?.displayName}</a>
-              </Navbar.Text>
-              <Navbar.Text>
+              </div>
+              <div className="d-flex justify-content-center align-items-center"> 
               {
                   user?.email? 
                   <Button className="text-white fw-bold ms-3" onClick={logOut} variant="danger">Sign out </Button>:
                   <Nav.Link className="text-white" as={Link} to="/login"><Button variant="success fw-bold">Sign In </Button></Nav.Link>
               }
-              </Navbar.Text>
+              </div>
             </Nav>
           </Navbar.Collapse>
         </Container>
